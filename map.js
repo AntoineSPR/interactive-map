c571b1c6d1898467f0d03475a92f89d63354bcf4
AntoineSPR <antoine.simper@gmail.com>
2024-06-25 19:09:55 +0200

    Add JSON file and iterate over each entry to insert markers

--- map.js
+++ map.js
@@ -1,5 +1,24 @@
 let map = document.querySelector("gmp-map");
-let marker = document.createElement("gmp-advanced-marker");
-map.appendChild(marker);
-marker.setAttribute("position", "48.864716, 2.349014");
-marker.setAttribute("title", "Paris");
+
+function loadJSON(callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open('GET', 'cities.json');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(xhr.responseText);
+    }
+  }
+  xhr.send();
+};
+
+loadJSON(function(response) {
+  let cities = JSON.parse(response);
+  cities.forEach(function(city) {
+    let marker = document.createElement("gmp-advanced-marker");
+    marker.setAttribute("position", `${city.coordinates.lat}, ${city.coordinates.lng}`);
+    marker.setAttribute("title", city.name);
+    map.appendChild(marker);
+    console.log(city);
+  });
+});
