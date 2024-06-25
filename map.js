66752ff4736d8aa20cddf2dfaad24661c94b0618
AntoineSPR <antoine.simper@gmail.com>
2024-06-25 20:37:44 +0200

    Add a way to close the legend

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 let map = document.querySelector("gmp-map");
 let list = document.querySelector(".list");
+let legend = document.getElementById("legend");
 
 function loadJSON(callback) {
   let xhr = new XMLHttpRequest();
@@ -15,12 +16,19 @@ function loadJSON(callback) {
 
 function displayLegend(event, city){
   legend.innerHTML = 
-    `<h2>${city.name}</h2>*
-    <p>Population: ${city.population}</p>`;
+    `<h2>${city.name}</h2>
+    <p>Population: ${city.population}</p>
+    <span>&#10006;</span>`;
   legend.style.display = "block";
   legend.style.left = event.clientX + "px";
   legend.style.top = event.clientY + "px";
+  let cross = document.querySelector("#legend span");
+  cross.addEventListener("click", () => {
+    legend.style.display = "none";
+  })
 }
+
+
 
 loadJSON(function(response) {
   let cities = JSON.parse(response);
