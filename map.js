025153fee58c9be3bc0c914d4376a380ac863c3c
AntoineSPR <antoine.simper@gmail.com>
2024-06-25 18:37:28 +0200

    Initialize map with markers and insert additional marker

---
+++ map.js
@@ -0,0 +1,5 @@
+let map = document.querySelector("gmp-map");
+let marker = document.createElement("gmp-advanced-marker");
+map.appendChild(marker);
+marker.setAttribute("position", "48.864716, 2.349014");
+marker.setAttribute("title", "Paris");
